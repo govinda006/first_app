f7cdd3d7943e856a252066b9cbf95d31d339509f
Govinda Prasad Parida <govindaprasadparida2@gmail.com>
2024-02-01 03:14:26 +0530

    Updated

---
+++ lib/dice_roller.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/dice-1.png';
+
+  void rollDice() {
+    setState(() {
+      activeDiceImage = 'assets/dice-3.png';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            // padding: const EdgeInsets.only(
+            //   top: 20,
+            // ),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 26,
+            ),
+          ),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
